dfcbcfd39def7fa0913f3d8f4cce6232f3e7df28
aloopuri <thassan577@gmail.com>
2023-05-10 23:00:43 +0100

    added repeatable games. this took way too long

--- src/game.js
+++ src/game.js
@@ -2,11 +2,95 @@ var title = "My Balloon Game"
 // hoisting is the difference between let and var
 let developer = "Talal"
 
-const BALLOON_TOTAL = 20
+const BALLOON_TOTAL = 10
 
 const balloons = []
 
 let score = 0
+
+function greeting() {
+    // let gameTitle = title + " - " + "by " + developer
+    // interpolated string
+    let gameTitleText = `${title} - by ${developer}`
+
+    let gameTitle = document.getElementById("game-title")
+    gameTitle.innerHTML = gameTitleText
+}
+
+
+function startGame() {
+    buildBalloons()
+    let btn = document.getElementById("game-button")
+    document.getElementById("game-canvas").style.display = "inline"
+    btn.innerHTML = "Restart Game"
+    btn.onclick = restartGame
+}
+
+function buildBalloons() {
+    for (let i = 0; i < BALLOON_TOTAL; i++) {
+        balloons.push(new Balloon(random(width), random(height), 33, color(random(255), random(255), random(255))))
+    }
+}
+
+function setup() {
+    makeCanvas()
+    document.getElementById("game-canvas").style.display = "none"
+
+}
+    
+function draw() {
+    //a nice sky blue background
+    background(135, 206, 235)
+
+    for(let balloon of balloons) {
+        balloon.blowAway()
+        balloon.checkToPop()
+        fill(balloon.col)
+        circle(balloon.x, balloon.y, balloon.r)
+    }
+
+    if (score == BALLOON_TOTAL) {
+        youWin()
+    }
+}
+
+function youWin() {
+    noLoop()
+
+    let para = document.createElement("p")
+    para.style.fontSize = "64px"
+    let textNode = document.createTextNode("You Win!")
+    para.appendChild(textNode)
+    para.setAttribute("id", "win-message")
+
+    document.getElementById("game-container").appendChild(para)
+    document.getElementById("game-canvas").style.display = "none"
+}
+
+function restartGame() {
+    if (document.getElementById("win-message")) {
+        const elementToDelete = document.getElementById("win-message");
+        elementToDelete.parentNode.removeChild(elementToDelete);
+    }
+    score = 0
+    document.getElementById("score").innerHTML = score
+    loop()
+    document.getElementById("game-canvas").style.display = "inline"
+
+    balloons.splice(0, balloons.length)
+    buildBalloons()
+
+}
+
+function makeCanvas() {
+    //creates canvas object and attaches it to specified container
+    let canvas = createCanvas(640, 480)
+    canvas.parent("game-container") 
+    document.querySelector("#game-container canvas").setAttribute("id", "game-canvas")
+}
+
+/////////////////////////////////////////////////
+// extra notes and stuff
 
 // balloons.push("A new balloon")
 // balloons.push(1)
@@ -31,50 +115,3 @@ let score = 0
 // testBalloon.move = function() {
 //     this.x += 10
 // }
-
-function greeting() {
-    // let gameTitle = title + " - " + "by " + developer
-    let gameTitleText = `${title} - by ${developer}`
-
-    let gameTitle = document.getElementById("game-title")
-    gameTitle.innerHTML = gameTitleText
-}
-
-function setup() {
-    //creates canvas object and attaches it to specified container
-    let canvas = createCanvas(640, 480)
-    canvas.parent("game-container")
-
-    for (let i = 0; i < BALLOON_TOTAL; i++) {
-        balloons.push(new Balloon(random(width), random(height), 33, color(random(255), random(255), random(255))))
-    }
-    
-}
-    
-function draw() {
-    //a nice sky blue background
-    background(135, 206, 235)
-
-    for(let balloon of balloons) {
-        balloon.blowAway()
-        balloon.checkToPop()
-        fill(balloon.col)
-        circle(balloon.x, balloon.y, balloon.r)
-    }
-
-    if (score ==  BALLOON_TOTAL) youWin()
-}
-
-function youWin() {
-    noLoop()
-
-    let para = document.createElement("p")
-    para.style.fontSize = "64px"
-    let textNode = document.createTextNode("You Win!")
-    para.appendChild(textNode)
-
-    document.getElementById("game-container").appendChild(para)
-
-    let canvas = document.querySelector("#game-container canvas")
-    canvas.remove()
-}
